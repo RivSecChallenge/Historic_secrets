4e64450e960b40291a4ca1e7e3bbbd6024863214
Simen <103125846+Simen-RivSec-Challenge@users.noreply.github.com>
2022-04-07 14:08:22 +0200

    Basic webpage

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", root)
+
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func root(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello world \n")
 }

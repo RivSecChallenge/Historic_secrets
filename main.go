0abe278e64c0ca49d149d234a47e7023d4baabbb
Simen <103125846+Simen-RivSec-Challenge@users.noreply.github.com>
2022-04-07 14:08:22 +0200

    Added more functionallity

--- main.go
+++ main.go
@@ -21,8 +21,26 @@ func main() {
 }
 
 func root(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		rootPost(w, req)
+	} else {
+		rootGet(w, req)
+	}
+}
+
+func rootGet(w http.ResponseWriter, req *http.Request) {
 	t, err := template.ParseFiles("static/index.html")
 	check(err)
 	err = t.Execute(w, template.HTML(""))
 	check(err)
+}
+
+func rootPost(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+
+	//Consider handeling missing data
+	value := req.Form["secret"][0]
+	if value == "Spring2022" {
+		fmt.Fprintln(w, "Put secret word here")
+	}
 }
